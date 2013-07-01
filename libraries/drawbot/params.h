e71c8d9c20b4505cb7115fd5d4dc709cf6960b53
Nathanael Jourdane <roipoussiere@gmail.com>
2013-07-01 09:35:55 +0200

    renommage des #define en anglais

--- libraries/drawbot/params.h
+++ libraries/drawbot/params.h
@@ -1,46 +1,56 @@
 /*
 This work is licensed under the Creative Commons Attribution
 - Pas d’Utilisation Commerciale - Partage dans les Mêmes Conditions
-3.0 France License. To view a copy of this license, visit
-http://creativecommons.org/licenses/by-nc-sa/3.0/fr/.
+3.0 France License. To view a copy of this license, see COPYING.txt
+file or visit http://creativecommons.org/licenses/by-nc-sa/3.0/fr/.
 */
 
 /**********************
 * Définition des pins *
 ***********************/
 
-// Ne pas prendre les pins 0 et 1 qui sont utilisées par le port série (communication avec Processing)
+// Pins 0 et 1 : utilisées par le port série (communication avec Processing)
 
-// pin de commande du servo-moteur
-#define PIN_SERVO 3
-
-// pin pour le bouton play/pause
-#define PIN_BP 4
+// Capteurs fin de course
+#define PIN_LEFT_CAPTOR 2 // gauche
+#define PIN_RIGHT_CAPTOR 3 // droite
 
 // desactivation des moteurs (pin /en sur pololu)
-#define PIN_OFF_MOT 5
+#define PIN_OFF_MOTORS 4
 
-// direction des moteurs
-#define PIN_DIR_MOT_G 6 // moteur gauche
-#define PIN_DIR_MOT_D 7 // moteur droit
+// pin de commande du servo-moteur
+#define PIN_SERVO 5
 
-// vitesse des moteurs
-#define PIN_CLK_MOT_G 8 // moteur gauche
-#define PIN_CLK_MOT_D 9 // moteur droit
+// moteur gauche
+#define PIN_LEFT_MOTOR_SPEED 6 // vitesse
+#define PIN_LEFT_MOTOR_DIRECTION 7 // direction
 
-// pin CS de la carte sd
-#define PIN_CS 10
-// shield Ethernet Arduino : pin 4
-// shield et module SD Adafruit : pin 10
-// shield SD Snootlab : pin 10
-// shield SD Sparkfun : pin 8
+// moteur droit
+#define PIN_RIGHT_MOTOR_SPEED 8 // vitesse
+#define PIN_RIGHT_MOTOR_DIRECTION 9 // direction
 
+// pin CS de la carte SD
+// Snootlab et Adafruit : 10 - Sparkfun : 8
+#define PIN_SD_CS 10
+
+// Pins 11, 12 et 13 : utilisés par la carte SD (MOSI, MISO, SCK)
+
+// Diode IR pour télécommande
+#define PIN_REMOTE A0
+
+// Écran LCD
+#define PIN_SCREEN_SCE A1
+#define PIN_SCREEN_RST A2
+#define PIN_SCREEN_DC A3
+#define PIN_SCREEN_SDIN A4
+#define PIN_SCREEN_SCLK A5
 
 /*************
 * paramètres *
 **************/
 
 // servo-moteur :
+
 #define MIN_SERVO 30 // position lorsque le robot n'écrit pas (en degrés)
 #define MAX_SERVO 90 // position lorsque le robot écrit (en degrés)
 
@@ -50,7 +60,20 @@ http://creativecommons.org/licenses/by-nc-sa/3.0/fr/.
 // moteurs :
 
 // nombre de pas
-#define NBPAS 200
+#define STEPS 800
 
 // Diamètre du pignon
-#define DIAMETRE 12
+#define DIAMETER 12
+
+// Direction des moteurs
+#define LEFT_DIRECTION true
+#define RIGHT_DIRECTION true
+
+/*********************
+* Valeurs par défaut *
+**********************/
+
+#define SCALE 1.0
+#define SPEED_SCALE 1.0
+
+#define DEFAULT_SPEED 10
