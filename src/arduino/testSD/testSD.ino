ba5bd3080a935fc7bd119174d8f2455679a32f4b
Victor Adam <Victor.Adam@derpymail.org>
2014-05-22 23:33:43 +0200

    Licensing

--- src/arduino/testSD/testSD.ino
+++ src/arduino/testSD/testSD.ino
@@ -1,4 +1,10 @@
 /*
+ * This file is part of Drawall, a vertical tracer (aka drawbot) - see http://drawall.fr/
+ * Drawall is free software and licenced under GNU GPL v3 : http://www.gnu.org/licenses/
+ * Copyright (c) 2012-2014 Nathanaël Jourdane
+ */
+
+/*
   SD card test 
    
  This example shows how use the utility libraries on which the'
